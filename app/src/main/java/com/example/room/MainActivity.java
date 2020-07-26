2a18a77c857ce9546a6eb72ed75f54bd5d3f7a40
yangchen <chen.yang1@thoughtworks.com>
2020-07-26 14:14:26 +0800

    add onclick event to button on main activity

--- app/src/main/java/com/example/room/MainActivity.java
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.room;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_main_submit)
+    void goSubmitActivity() {
+        Intent intent = new Intent(this, SubmitActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.button_main_view)
+    void goViewActivity() {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
     }
 }
