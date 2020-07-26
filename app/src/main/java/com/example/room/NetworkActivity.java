4c74ed1add3ace5feff3f7c25548b01890a5e782
yangchen <chen.yang1@thoughtworks.com>
2020-07-26 18:09:13 +0800

    add button to get person info

--- app/src/main/java/com/example/room/NetworkActivity.java
+++ app/src/main/java/com/example/room/NetworkActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class NetworkActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.net_button_get_info)
+    void getPersonInfo(){
+
     }
 }
