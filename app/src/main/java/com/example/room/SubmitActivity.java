109f95fd784bd8821d237b5c687ba8ccd20ed2e1
yangchen <chen.yang1@thoughtworks.com>
2020-07-26 14:45:41 +0800

    check person info when click button submit

--- app/src/main/java/com/example/room/SubmitActivity.java
+++ app/src/main/java/com/example/room/SubmitActivity.java
@@ -1,14 +1,51 @@
 package com.example.room;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SubmitActivity extends AppCompatActivity {
+
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+
+    @BindView(R.id.sub_edit_name)
+    EditText mName;
+
+    @BindView(R.id.sub_edit_gender)
+    EditText mGender;
+
+    @BindView(R.id.sub_edit_age)
+    EditText mAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.sub_button_submit)
+    void submitPersonInfo() {
+        if (!checkPersonInfo()){
+            Toast.makeText(this,"失败",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkPersonInfo() {
+        if (TextUtils.isEmpty(mName.getText()) || TextUtils.isEmpty(mGender.getText()) || TextUtils.isEmpty(mAge.getText())) {
+            return false;
+        }
+        int gender = Integer.parseInt(mGender.getText().toString());
+        int age = Integer.parseInt(mAge.getText().toString());
+        return age >= 0 && (gender == MALE || gender == FEMALE);
     }
 }
