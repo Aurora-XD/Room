8604622ed4364aa3b2c5448a3de5e2b048b1d450
yangchen <chen.yang1@thoughtworks.com>
2020-07-26 14:04:55 +0800

    fix localdataSource

--- app/src/main/java/com/example/room/LocalDataSource.java
+++ app/src/main/java/com/example/room/LocalDataSource.java
@@ -3,7 +3,7 @@ package com.example.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {},version = 1)
+@Database(entities = {Person.class},version = 1)
 public abstract class LocalDataSource extends RoomDatabase {
     public abstract PersonDao getPersonDao();
 }
