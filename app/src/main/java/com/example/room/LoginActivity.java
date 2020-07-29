e566919005389ed92411af5a65c8f287c02bd56a
yangchen <chen.yang1@thoughtworks.com>
2020-07-29 18:06:59 +0800

    refactor code

--- app/src/main/java/com/example/room/LoginActivity.java
+++ app/src/main/java/com/example/room/LoginActivity.java
@@ -12,20 +12,11 @@ import androidx.lifecycle.ViewModelProvider;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 import static com.example.room.Result.LOGIN_SUCCESSFULLY;
 import static com.example.room.Result.PASSWORD_INVALID;
-import static com.example.room.Result.USER_NAME_NOT_EXIST;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private CompositeDisposable compositeDisposable;
 
     private LoginViewModel loginViewModel;
 
@@ -41,18 +32,34 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
-        compositeDisposable = new CompositeDisposable();
 
         loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+        loginViewModel.observeInsertResult(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                Toast.makeText(MyApplication.getContext(), aBoolean ? "Insert successfully!" : "Insert Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        loginViewModel.observeUserInfoIsInvalid(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                Log.d("onChanged", "checkUserInfo: " + aBoolean);
+                if (!aBoolean) {
+                    Toast.makeText(MyApplication.getContext(), "Invalid user info", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        loginViewModel.observeLoginResult(this, new Observer<LoginResult>() {
             @Override
             public void onChanged(LoginResult loginResult) {
-                if(LOGIN_SUCCESSFULLY.equals(loginResult.getResult())){
+                if (LOGIN_SUCCESSFULLY.equals(loginResult.getResult())) {
                     Toast.makeText(MyApplication.getContext(), "Login successfully", Toast.LENGTH_SHORT).show();
-                }else if (PASSWORD_INVALID.equals(loginResult.result)){
+                } else if (PASSWORD_INVALID.equals(loginResult.result)) {
                     Toast.makeText(MyApplication.getContext(), "Password is invalid", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     Toast.makeText(MyApplication.getContext(), "Username does not exist", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -61,63 +68,16 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.login_button_insert_user)
     void insertUser() {
-        Disposable subscribe = loginViewModel.insertUser()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(Long aLong) throws Exception {
-                        Toast.makeText(MyApplication.getContext(), "Insert successfully!", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        compositeDisposable.add(subscribe);
+        loginViewModel.insertUser();
     }
 
     @OnClick(R.id.login_button_login)
     void login() {
-        boolean checkUserInfo = loginViewModel.checkUserInfo(mUsername.getText().toString(), mPassword.getText().toString());
-        Log.d("TAG", "login: " + checkUserInfo);
-        if (!checkUserInfo) {
-            Toast.makeText(this, "Invalid user info", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        Disposable subscribe = loginViewModel.login(mUsername.getText().toString())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<User>() {
-                    @Override
-                    public void accept(User user) throws Exception {
-                        LoginResult loginResult = new LoginResult();
-                        Log.d("TAG", "accept: " + user.getUsername() + user.getPassword());
-                        if (user.getPassword().equals(mPassword.getText().toString())) {
-                            loginResult.setResult(LOGIN_SUCCESSFULLY);
-                        } else {
-                            loginResult.setResult(PASSWORD_INVALID);
-                        }
-                        loginViewModel.getLoginResult().setValue(loginResult);
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-
-                    }
-                }, new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        LoginResult loginResult = new LoginResult();
-                        loginResult.setResult(USER_NAME_NOT_EXIST);
-                        loginViewModel.getLoginResult().setValue(loginResult);
-                    }
-                });
-
-        compositeDisposable.add(subscribe);
+        loginViewModel.login(mUsername.getText().toString(), mPassword.getText().toString());
     }
 
     @Override
     protected void onDestroy() {
-        compositeDisposable.clear();
         super.onDestroy();
     }
 }
--- app/src/main/java/com/example/room/LoginViewModel.java
+++ app/src/main/java/com/example/room/LoginViewModel.java
@@ -1,36 +1,110 @@
 package com.example.room;
 
 import android.text.TextUtils;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
-import java.util.Objects;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
-import io.reactivex.Maybe;
+import static com.example.room.Result.LOGIN_SUCCESSFULLY;
+import static com.example.room.Result.PASSWORD_INVALID;
+import static com.example.room.Result.USER_NAME_NOT_EXIST;
 
 public class LoginViewModel extends ViewModel {
     private MutableLiveData<LoginResult> loginResult;
+    private MutableLiveData<Boolean> insertResult;
+    private MutableLiveData<Boolean> isUserInfoInvalid;
     private UserRepository userRepository = new UserRepository();
+    private CompositeDisposable compositeDisposable;
 
-    public Maybe<Long> insertUser() {
-        return userRepository.saveUser(new User("android", "123456"));
+    public LoginViewModel() {
+        compositeDisposable = new CompositeDisposable();
+        loginResult = new MutableLiveData<>();
+        insertResult = new MutableLiveData<>();
+        isUserInfoInvalid = new MutableLiveData<>();
     }
 
-    public MutableLiveData<LoginResult> getLoginResult() {
-        if (Objects.isNull(loginResult)) {
-            loginResult = new MutableLiveData<>();
+    public void observeInsertResult(@NonNull LifecycleOwner owner, @NonNull Observer<Boolean> observer){
+        insertResult.observe(owner, observer);
+    }
+
+    public void observeUserInfoIsInvalid(@NonNull LifecycleOwner owner, @NonNull Observer<Boolean> observer){
+        isUserInfoInvalid.observe(owner, observer);
+    }
+
+    public void observeLoginResult(@NonNull LifecycleOwner owner, @NonNull Observer<LoginResult> observer){
+        loginResult.observe(owner, observer);
+    }
+
+    public void insertUser() {
+        Disposable insertDisposable = userRepository.saveUser(new User("android", "123456"))
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        insertResult.postValue(true);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        insertResult.postValue(false);
+                    }
+                });
+        compositeDisposable.add(insertDisposable);
+    }
+
+    public void login(String name,String password) {
+        if(!checkUserInfo(name,password)){
+            return;
         }
-        return loginResult;
-    }
+        Disposable loginDisposable = userRepository.findByUserName(name).subscribeOn(Schedulers.io())
+                .subscribe(new Consumer<User>() {
+                    @Override
+                    public void accept(User user) throws Exception {
+                        LoginResult result = new LoginResult();
+                        Log.d("TAG", "accept: " + user.getUsername() + user.getPassword());
+                        if (user.getPassword().equals(password)) {
+                            result.setResult(LOGIN_SUCCESSFULLY);
+                        } else {
+                            result.setResult(PASSWORD_INVALID);
+                        }
+                        loginResult.postValue(result);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
 
-    public Maybe<User> login(String name) {
-        return userRepository.findByUserName(name);
+                    }
+                }, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        LoginResult result = new LoginResult();
+                        result.setResult(USER_NAME_NOT_EXIST);
+                        loginResult.postValue(result);
+                    }
+                });
+        compositeDisposable.add(loginDisposable);
     }
-
 
     public boolean checkUserInfo(String username, String password) {
-        return !TextUtils.isEmpty(username) && !TextUtils.isEmpty(password);
+        boolean isInvalid = !TextUtils.isEmpty(username) && !TextUtils.isEmpty(password);
+        isUserInfoInvalid.postValue(isInvalid);
+        Log.d("TAG", "checkUserInfo: "+isInvalid);
+        return isInvalid;
     }
 
+    @Override
+    protected void onCleared() {
+        compositeDisposable.clear();
+        super.onCleared();
+    }
 }
